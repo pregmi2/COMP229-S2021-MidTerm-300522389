5ff32703cc93d7e329db711ef545ab5974bfab81
pregmi2 <pregmi2@my.centennialcollege.ca>
2021-06-26 10:30:11 -0400

    second commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+/**
+ * Add your code here to fix URI connection
+ * 
+ */
+ module.exports =
+ {
+     "URI": "mongodb+srv://midterm:ZfqOw6LCmHCGgsyJ@cluster0.ndlf4.mongodb.net/book_store?retryWrites=true&w=majority"
+ }
